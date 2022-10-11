29d89478453e064bd8452f526532e24d4dad7bef
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2022-10-11 15:53:26 +0200

    Build learn-ocaml (ocamlsf/learn-ocaml:master)

---
+++ docs/js/learnocaml-config.js
@@ -0,0 +1,8 @@
+var learnocaml_config = {
+  enableTutorials: false,
+  enablePlayground: true,
+  enableLessons: false,
+  enableExercises: true,
+  enableToplevel: true,
+  baseUrl: "https://erikmd.github.io/learn-ocaml-meetup-demo"
+}
